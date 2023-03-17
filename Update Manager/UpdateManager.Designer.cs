a8a0e2a1ef9301e37481fa9edb64376e808ea6bf
eugen <eugenereybulahan@gmail.com>
2023-03-18 02:13:24 +0800

    feat: installer

---
+++ Update Manager/UpdateManager.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Update_Manager
+{
+    partial class UpdateManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_install = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(60, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check for Updates";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(341, 217);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btn_install
+            // 
+            this.btn_install.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_install.Location = new System.Drawing.Point(188, 254);
+            this.btn_install.Name = "btn_install";
+            this.btn_install.Size = new System.Drawing.Size(122, 23);
+            this.btn_install.TabIndex = 3;
+            this.btn_install.Text = "Install";
+            this.btn_install.UseVisualStyleBackColor = true;
+            this.btn_install.Click += new System.EventHandler(this.btn_install_Click);
+            // 
+            // UpdateManager
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(365, 316);
+            this.Controls.Add(this.btn_install);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "UpdateManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Update Manager";
+            this.Load += new System.EventHandler(this.UpdateManager_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_install;
+    }
+}
+
---
+++ Update Manager/UpdateManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net.Http;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace Update_Manager
+{
+    public partial class UpdateManager : Form
+    {
+        public UpdateManager()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateManager_Load(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            // When the user clicks the "Install" button, mark the corresponding file as "installing"
+            if (e.ColumnIndex == dataGridView1.Columns["InstallColumn"].Index && e.RowIndex >= 0)
+            {
+                DataGridViewCheckBoxCell cell = dataGridView1.Rows[e.RowIndex].Cells["InstallColumn"] as DataGridViewCheckBoxCell;
+                if ( cell != null)
+                {
+                    if (cell.Value == cell.TrueValue)
+                    {
+                        cell.Value = cell.FalseValue;
+                    }
+                    else
+                    {
+                        cell.Value = cell.TrueValue;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+              
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {      
+                // Send a request to the URL of your website and retrieve the HTML content of the page
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://www.pythonanywhere.com/user/codefilter/files/home/codefilter/Inventory/Update_Manager_APP.msi");
+                string content = await response.Content.ReadAsStringAsync();
+
+                // Use HtmlAgilityPack to extract the list of files available for download
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(content);
+
+                List<InventoryFile> fileList = new List<InventoryFile>();
+                foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    string href = link.Attributes["href"].Value;
+                    if (href.EndsWith(".msi"))
+                    {
+                        fileList.Add(new InventoryFile() { FileName = href, Install = false });
+                    }
+                }
+
+                // Populate the DataGridView control with the list of files
+                dataGridView1.AutoGenerateColumns = false;
+                dataGridView1.DataSource = fileList;
+
+            }
+            catch( Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }  
+        }
+        private void btn_install_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class InventoryFile
+    {
+        public string FileName { get; set; }
+        public bool Install { get; set; }
+    }
+}
